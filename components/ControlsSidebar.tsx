25d2465567d00b65153c90a1fec8b85233690c01
neducod <nedunony16@gmail.com>
2026-09-15 03:10:36 +0100

    updates

--- components/ControlsSidebar.tsx
+++ components/ControlsSidebar.tsx
@@ -94,7 +94,7 @@ export function ControlsSidebar({ state, updateDetails, updateStyle, updateGradi
           <textarea
             value={details.note ?? ""}
             onChange={(e) => updateDetails({ note: e.target.value })}
-            className="input min-h-[70px] resize-none text-red-600"
+            className="input min-h-[70px] resize-none text-black"
             placeholder="Come ready to dance!"
           />
         </Field>
