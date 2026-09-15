3a7b7e7aaa682a6a3b0598265ec53c31a6933f80
neducod <nedunony16@gmail.com>
2026-09-15 02:59:37 +0100

    deleted the extra field

--- components/ControlsSidebar.tsx
+++ components/ControlsSidebar.tsx
@@ -81,14 +81,14 @@ export function ControlsSidebar({ state, updateDetails, updateStyle, updateGradi
           />
         </Field>
 
-        <Field label="Invitation">
+        {/* <Field label="Invitation">
           <input
             type="text"
-            value={details.Invitation ?? ""}
-            onChange={(e) => updateDetails ({invitation: e.target.value})}
+            value={details.invitation ?? ""}
+            onChange={(e) => updateDetails({ invitation: e.target.value })}
             className="input text-black"
             placeholder="Please come alone"
-        </Field>
+        </Field> */}
 
         <Field label="Custom Note">
           <textarea
