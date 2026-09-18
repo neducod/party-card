436cabf31ac12c1e48e3f0aabc4eddd80fa3f5d6
neducod <nedunony16@gmail.com>
2026-09-18 12:55:50 +0100

    repsonsive design

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ import { ExportBar } from "@/components/ExportBar";
 export default function Home() {
   const cardState = useCardState();
   const previewRef = useRef<HTMLDivElement>(null);
-
+//flex h-screen w-full flex-col md:flex-row
   return (
-    <main className="flex h-screen w-full flex-col md:flex-row">
+    <main className="flex min-h-screen w-full flex-col md:h-screen md:flex-row">
       <ControlsSidebar
         state={cardState.state}
         updateDetails={cardState.updateDetails}
@@ -19,12 +19,10 @@ export default function Home() {
         updateGradient={cardState.updateGradient}
         reset={cardState.reset}
       />
-
-      <section className="flex flex-1 flex-col items-center justify-center gap-6 bg-neutral-100 p-8">
+      {/*flex flex-1 flex-col items-center justify-center gap-6 bg-neutral-100 p-8  */}
+      <section className="flex flex-1 flex-col items-center justify-start gap-4 bg-neutral-100 p-4 py-6 md:justify-center md:p-8">
         <CardPreview ref={previewRef} state={cardState.state} />
-        <ExportBar 
-  targetRef={previewRef as React.RefObject<HTMLDivElement>} 
-  fileName={cardState.state.details.eventName || "invitation"} 
+        <ExportBar targetRef={previewRef as React.RefObject<HTMLDivElement>} fileName={cardState.state.details.eventName || "invitation"} 
 />
         {/* <ExportBar targetRef={previewRef} fileName={cardState.state.details.eventName || "invitation"} /> */}
       </section>
